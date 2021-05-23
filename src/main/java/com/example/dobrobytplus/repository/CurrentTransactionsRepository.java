8f93509f1412b3793b9ba94c8832316a5516b777
Anna Kelm <annamariakelm@gmail.com>
2021-05-23 03:43:35 +0200

    CurrentTransactionsService i dodatki do CurrentTransactionsRepository

--- src/main/java/com/example/dobrobytplus/repository/CurrentTransactionsRepository.java
+++ src/main/java/com/example/dobrobytplus/repository/CurrentTransactionsRepository.java
@@ -1,5 +1,6 @@
 package com.example.dobrobytplus.repository;
 
+import com.example.dobrobytplus.entities.Accounts;
 import com.example.dobrobytplus.entities.CurrentTransactions;
 import com.example.dobrobytplus.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,9 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.Date;
 import java.util.List;
 
-/**
- * tutaj dajemy metody pobierania danych itp customowego zapisu
- */
+
 public interface CurrentTransactionsRepository extends JpaRepository<CurrentTransactions, Long> {
     List<CurrentTransactions> findByUserUsername(String username);
 
@@ -20,4 +19,10 @@ public interface CurrentTransactionsRepository extends JpaRepository<CurrentTran
 
     @Query("SELECT sum(ct.value) FROM CurrentTransactions ct WHERE ct.value < 0 AND ct.user = :user AND ct.time BETWEEN :time_start AND :time_end")
     Double sumExpensesForUser(@Param("user") Users user, @Param("time_start") Date time_start, @Param("time_end") Date time_end);
+
+    List<CurrentTransactions> findCurrentTransactionsByAccount_IdAccounts(Long icAccounts);
+
+    @Query("SELECT sum(ct.value) FROM CurrentTransactions ct WHERE ct.account = :account")
+    Double sumAccount(@Param("account") Accounts account);
+
 }
---
+++ src/main/java/com/example/dobrobytplus/service/CurrentTransactionsService.java
@@ -0,0 +1,84 @@
+package com.example.dobrobytplus.service;
+
+import com.example.dobrobytplus.dto.AccountsDto;
+import com.example.dobrobytplus.dto.CurrentTransactionsDto;
+import com.example.dobrobytplus.dto.OperationDto;
+import com.example.dobrobytplus.entities.*;
+import com.example.dobrobytplus.exceptions.UserHasNoAccess;
+import com.example.dobrobytplus.repository.AccountsRepository;
+import com.example.dobrobytplus.repository.CurrentTransactionsRepository;
+import com.example.dobrobytplus.repository.PermissionsRepository;
+import com.example.dobrobytplus.repository.UsersRepository;
+import com.example.dobrobytplus.security.MyUsersPrincipal;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class CurrentTransactionsService {
+    private final PermissionsRepository permissionsRepository;
+    public final AccountsRepository accountsRepository;
+    private final CurrentTransactionsRepository currentTransactionsRepository;
+    private final UsersRepository usersRepository;
+
+
+    private String getAuthenticatedUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ((MyUsersPrincipal) principal).getUsername();
+    }
+
+    public List<CurrentTransactionsDto> getCurrentTransactions(Long idAccount) {
+        List<CurrentTransactions> currentTransactions = currentTransactionsRepository.findCurrentTransactionsByAccount_IdAccounts(idAccount);
+        return currentTransactions
+                .stream()
+                .map(CurrentTransactionsDto::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<Boolean> canUserDelete(Long idAccount, List<CurrentTransactionsDto> currentTransactions) {
+        String username = getAuthenticatedUsername();
+        List<Permissions> permission = permissionsRepository.findByUserUsernameAndAccount_IdAccounts(username, idAccount);
+        if (permission.size()<1) {
+            throw new UserHasNoAccess();
+        }
+        PermissionTypes permissionType = permission.get(0).getPermissionTypes();
+
+        if (permissionType == PermissionTypes.CHILD) {
+            // tylko jego wlasne
+            return currentTransactions.stream()
+            .map(x -> x. getUsername().equals(username))
+                    .collect(Collectors.toList());
+        } else {
+            return new ArrayList<>(Collections.nCopies(currentTransactions.size(), true));
+        }
+
+    }
+
+    public Double sumCurrentTransactionsPLN(Long idAccount){
+        Accounts account = accountsRepository.findByIdAccounts(idAccount);
+        return currentTransactionsRepository.sumAccount(account);
+    }
+
+    public Double plnToMikrosasin(double pln) {
+        return pln/70;
+    }
+
+    public void saveCurrentTransaction(CurrentTransactionsDto currentTransactionsDto) {
+        Accounts account = accountsRepository.findByIdAccounts(currentTransactionsDto.getIdAccount());
+        Users user = usersRepository.findByUsername(currentTransactionsDto.getUsername());
+        CurrentTransactions currentTransaction = new CurrentTransactions(currentTransactionsDto.getValue(),
+                currentTransactionsDto.getTime(),
+                currentTransactionsDto.getDescription(),
+                account,
+                user);
+        currentTransactionsRepository.saveAndFlush(currentTransaction);
+    }
+
+}
