1e464e734797cd46fc04a5c519a47375850ab422
KarolC <karolchy@ymail.com>
2021-05-26 14:27:38 +0200

    Nowe: "Aktualne transakcje": current_transactions.html + CurrentTransactionsController.java;
    uzupelnienie personal.html dla "Aktualne transakcje"

---
+++ src/main/java/com/example/dobrobytplus/controller/CurrentTransactionsController.java
@@ -0,0 +1,70 @@
+package com.example.dobrobytplus.controller;
+
+import com.example.dobrobytplus.dto.CurrentTransactionsDto;
+import com.example.dobrobytplus.exceptions.UserHasNoAccess;
+import com.example.dobrobytplus.service.*;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class CurrentTransactionsController {
+
+    @Autowired
+    private final AccountsService accountsService;
+    @Autowired
+    private final PermissionsService permissionsService;
+    @Autowired
+    private final CurrentTransactionsService currentTransactionsService;
+
+    @RequestMapping({"/current_transactions/{idAccount}"})
+    public String viewCurrentTransactionsPage(@PathVariable(name = "idAccount") Long idAccount, Model model) {
+        // jesli uzytkownik nie ma uprawnien, to wyrzucamy go na strone z bledem
+        //Long idAccount = (Long) modelMap.getAttribute("idAccount");
+        if (!permissionsService.doesCurrentUserHaveAccessToAccount(idAccount)) {
+            throw new UserHasNoAccess();
+        }
+
+        // ================================================================================
+
+        // typ rachunku i rola uzytkownika do wyswietlenia na stronie
+        String accountType = accountsService.getAccountType(idAccount);
+        model.addAttribute("accountType", accountType);
+
+        String userRole = permissionsService.currentUserRoleInAccount(idAccount);
+        model.addAttribute("userRole", userRole);
+
+        // ================================================================================
+
+        // CURRENT TRANSACTIONS
+
+        List<CurrentTransactionsDto> currentTransactions = currentTransactionsService.getCurrentTransactions(idAccount);
+        model.addAttribute("listCurrentTransactions", currentTransactions);
+
+        List<Boolean> canUserDeleteCurrentTransaction = currentTransactionsService.canUserDelete(idAccount, currentTransactions);
+        model.addAttribute("listCanUserDeleteCurrentTransaction", canUserDeleteCurrentTransaction);
+
+        Double saldoCurrentPLN = currentTransactionsService.sumCurrentTransactionsPLN(idAccount);
+        String strSaldoCurrentPLN = String.format("%.2f", saldoCurrentPLN);
+        model.addAttribute("strSaldoCurrentPLN", strSaldoCurrentPLN);
+
+        Double saldoCurrentMikroSasin = currentTransactionsService.plnToMikrosasin(saldoCurrentPLN);
+        String strSaldoCurrentMikroSasin = String.format("%.2f", saldoCurrentMikroSasin);
+        model.addAttribute("strSaldoCurrentMikroSasin", strSaldoCurrentMikroSasin);
+
+        return "current_transactions";
+    }
+
+    @RequestMapping("/deleteCurrentTransaction/{idCurrentTransaction}/idAcc/{idAccount}")
+    public String deleteCurrentTransaction(@PathVariable(name = "idCurrentTransaction") Long idCurrentTransaction, @PathVariable(name = "idAccount") Long idAccount) {
+        currentTransactionsService.deleteCurrentTransaction(idCurrentTransaction);
+
+        return "redirect:/current_transactions/{idAccount}";
+    }
+}
